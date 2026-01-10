9acaa75293d23e772e51332cf131888b3e7ff164
Rudra <rudra17122005@gmail.com>
2026-01-10 22:16:07 +0530

    chore(seo): update production domain to hotelsaugaatregency.in

--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
-    const baseUrl = 'https://www.hotelsaugaatregency.in' // Replace with your actual domain
+    const baseUrl = 'https://www.hotelsaugaatregency.in' // Deployed domain
 
     return {
         rules: {
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-    const baseUrl = 'https://www.hotelsaugaatregency.in'
+    const baseUrl = 'https://www.hotelsaugaatregency.in' // Deployed domain
 
     // Static routes
     const routes = [
